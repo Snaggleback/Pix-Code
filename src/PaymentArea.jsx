30d36eedabea0b49d10b7cb5c61170f06219a95f
Ítalo <lixoliteral@gmail.com>
2024-11-02 19:15:37 -0300

    Funcionalidade: Informativo de valor do Pix
    Os arquivos foram atualizadas para atender melhor às necessidades. Arquivos modificados: src/PaymentArea.jsx (tipo: arquivo, foi modificado)

--- src/PaymentArea.jsx
+++ src/PaymentArea.jsx
@@ -1,6 +1,7 @@
 import GenericButton from "./GenericButton";
 import ReadOnlyInput from "./ReadOnlyInput";
 import PIXCode from "../PIXCode";
+import { formatCurrency } from "./CurrencyInput";
 import { QRious } from "react-qrious";
 import { FaPix } from "react-icons/fa6";
 
@@ -22,19 +23,18 @@ export default function PaymentArea({ data, onBack }) {
     const copyPastePix = pixCode.generate();
 
     return (
-        <div className="text-center space-y-4 w-full flex flex-col">
-            <h1 className="text-2xl font-bold mt-2">
-                Valeu {firstName}!
-            </h1>
-            <p className="text-zinc-700 text-sm">
-                Este código Pix é de uso geral e não tem data de validade.
-            </p>
+        <div className="text-center space-y-4 w-full flex flex-col break-words">
+            <h1 className="text-2xl font-bold mt-2">Valeu {firstName}!</h1>
+            <h2 className="text-sm font-bold text-zinc-500">Pix gerado no valor de R$ {formatCurrency(value * 100)}</h2>
             <div className="relative">
                 <QRious value={copyPastePix} size={250} className="m-auto" />
                 <div className="absolute inset-0 w-full h-full flex">
                     <FaPix className="m-auto w-14 h-14 bg-white p-3 text-orange-500"></FaPix>
                 </div>
             </div>
+            <p className="text-zinc-700 text-sm">
+                Este código Pix pode ser pago quando quiser e quanto quiser.
+            </p>
             <h2 className="text-lg font-bold">Pix Copia e Cola</h2>
             <ReadOnlyInput defaultValue={copyPastePix} />
             <GenericButton className="w-full" onClick={onBack}>
